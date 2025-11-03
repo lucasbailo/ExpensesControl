99e8b34aa45b94051ee8c0f2c226045ee48a2bba
Lucas Bailo <lucasebailo@gmail.com>
2025-11-02 21:28:01 -0300

    create: adding Interfaces and Services

---
+++ ExpensesControl.Application/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using ExpensesControl.Application.DTOs;
+using ExpensesControl.Domain.Entities;
+
+namespace ExpensesControl.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<User> RegisterAsync(UserRegisterDto dto);
+        Task<User?> LoginAsync(UserLoginDto dto);
+        Task<User?> GetByEmailAsync(string email);
+    }
+}
--- ExpensesControl/Program.cs
+++ ExpensesControl/Program.cs
@@ -1,3 +1,5 @@
+using ExpensesControl.Application.Interfaces;
+using ExpensesControl.Application.Services;
 using ExpensesControl.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +13,8 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
