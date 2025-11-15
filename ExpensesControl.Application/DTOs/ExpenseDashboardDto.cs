d99f0aa6a4c03c934b21889d064be481cf20172f
Lucas Bailo <lucasebailo@gmail.com>
2025-11-15 18:13:09 -0300

    create: adding dashboardDto

---
+++ ExpensesControl.Application/DTOs/ExpenseDashboardDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesControl.Application.DTOs
+{
+    internal class ExpenseDashboardDto
+    {
+        public Dictionary<string, decimal> TotalByType { get; set; } = new();
+        public Dictionary<string, decimal> MonthlyTotals { get; set; } = new();
+    }
+}
